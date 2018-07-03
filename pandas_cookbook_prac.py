1308dc9e6763d5ca995abf1519d9b33ab617247a
unknown <jakinpilla@gmail.com>
2018-07-03 19:05:20 +0900

    studied

--- pandas_cookbook_prac.py
+++ pandas_cookbook_prac.py
@@ -471,5 +471,248 @@ msft_trailing_stop = set_trailing_loss('msft', '2017-6-1', '2018-6-1', .85)
 msft_trailing_stop.head()
 
 
+college = pd.read_csv('./data/college.csv', index_col='INSTNM')
+city = college['CITY']
+city.head()
+
+city.iloc[3]
+city.iloc[[10, 20, 30]]
+city.iloc[4:50:10]
+city.iloc[14]
+city.iloc[44]
+city.loc['Heritage Christian University']
+np.random.seed(1)
+labels = list(np.random.choice(city.index, 4))
+labels
+city.loc[labels]
+
+city.loc['Alabama State University':
+    'Reid State Technical College' : 10]
+
+college.iloc[99:102]
+college.iloc[99]
+college.iloc[101]
+start = 'International Academy of Hair Design'
+stop = 'Mesa Community College'
+college.loc[start:stop] # include last label value
+
+label = college.iloc[[60, 99, 3]].index.tolist()
+college.loc[label]
+college.iloc[[60, 99, 3]]
+
+## select rows and colunms at once
+
+college.iloc[:3, :4]
+college.loc[:'Amridge University', : 'MENONLY']
+college.iloc[:, [4, 6]].head()
+college.loc[:, ['WOMENONLY', 'SATVRMID']].head()
+college.iloc[[100, 200], [7, 5]]
+rows = ['GateWay Community College', 'American Baptist Seminary of the West']
+columns = ['SATMTMID', 'RELAFFIL']
+college.loc[rows, columns]
+college.head()
+college.iloc[5, -4]
+college.iloc[5] ## row = The University of Alabama
+college.columns[-4] ## columns = PCTFLOAN
+college.loc['The University of Alabama', 'PCTFLOAN']
+
+college.iloc[90:80:-2, 5]
+college.iloc[90:80:-2, :]
+college.iloc[82, :]
+start = 'Empire Beauty School-Flagstaff'
+stop = 'Arizona State University-Tempe'
+college.columns[5]
+college.loc[start:stop:-2, 'RELAFFIL']
+
+college.ix[:5, 'UGDS_WHITE':'UGDS_UNKN']
+col_start = college.columns.get_loc('UGDS_WHITE')
+col_start
+col_stop = college.columns.get_loc('UGDS_UNKN') + 1
+col_stop
+col_start, col_stop 
+
+# college.iloc[:5, col_start:col_stop] # error
+
+# .iat, .at
+college = pd.read_csv('./data/college.csv', index_col='INSTNM')
+cn = 'Texas A & M University-College Station'
+college.loc[cn, 'UGDS_WHITE']
+college.at[cn, 'UGDS_WHITE']
+
+# %timeit college.loc[cn, 'UGDS_WHITE']
+# %timeit college.at[cn, 'UGDS_WHITE']
+
+row_num = college.index.get_loc(cn)
+col_num = college.columns.get_loc('UGDS_WHITE')
+row_num, col_num
+# %timeit college.iloc[row_num, col_num]
+# %timeit college.iat[row_num, col_num]
+
+state = college['STABBR']
+state.iat[1000]
+state.at['Stanford University']
+
+# lazy row slicing
+
+college[10:20:2]
+city = college['CITY']
+city[10:20:2]
+start = 'Mesa Community College'
+stop = 'Spokane Community College'
+college[start:stop:1500]
+city[start:stop:1500]
+# college[:10, ['CITY', 'STABBR']] # unhashable type: 'slice'
+first_ten_instnm = college.index[:10]
+college.loc[first_ten_instnm, ['CITY', 'STABBR']]
+
+# slicing with dictionary order
+
+# college.loc['Sp': 'Su'] # error because not yet arranged
+college = college.sort_index()
+college.loc['Sp':'Su']
+college.loc['Sp':'Su'].index
+college.loc['S':'T']
+college.loc['S':'T'].index
+college = college.sort_index(ascending = False)
+college.index.is_monotonic_decreasing
+college.loc['E':'B']
+college.loc['E':'B'].index
+
+# Boolean indexing
+## select rows with boolean values
+
+movie = pd.read_csv('./data/movie.csv', index_col = 'movie_title')
+movie.head()
+movie_2_hours = movie['duration'] > 120
+movie_2_hours.head(10)
+movie_2_hours.sum()
+movie_2_hours.mean()
+movie['duration'].dropna().gt(120).mean()
+movie_2_hours.describe()
+movie_2_hours.value_counts(normalize = True)
+actors = movie[['actor_1_facebook_likes', 'actor_2_facebook_likes']].dropna()
+actors
+(actors['actor_1_facebook_likes'] > actors['actor_2_facebook_likes']).mean()
+
+# set multiful boolean condition 
+# imdb_score > 8, content_rating == 'PG-13', title_year < 2000 or > 2009
+
+movie = pd.read_csv('./data/movie.csv', index_col = 'movie_title')
+criteria1 = movie.imdb_score > 8
+criteria2 = movie.content_rating == 'PG-13'
+criteria3 = ((movie.title_year < 2000) | (movie.title_year > 2009))
+criteria3.head()
+
+criteria_final = criteria1 & criteria2 & criteria3
+criteria_final.head()
+
+crit_a1 = movie.imdb_score > 8
+crit_a2  = movie.content_rating == 'PG-13'
+crit_a3 = (movie.title_year < 2000) | (movie.title_year > 2009)
+final_crit_a = crit_a1 & crit_a2 & crit_a3
+
+crit_b1 = movie.imdb_score < 5
+crit_b2 = movie.content_rating == 'R'
+crit_b3 = (movie.title_year >= 2000) & (movie.title_year <= 2010)
+final_crit_b = crit_b1 & crit_b2 & crit_b3
+
+final_crit_all = final_crit_a | final_crit_b # make final boolean series
+final_crit_all.head()
+movie[final_crit_all].head() # deliver final boolean series to dataframe
+
+cols = ['imdb_score', 'content_rating', 'title_year']
+movie_filtered = movie.loc[final_crit_all, cols] # deliver final boolean series to dataframe with .loc
+movie_filtered.head()
+
+final_crit_a2 = (movie.imdb_score > 8) &\
+(movie.content_rating == 'PG-13') &\
+((movie.title_year < 2000) | (movie.title_year > 2009))
+
+final_crit_a2.equals(final_crit_a)
+
+# using index like boolean index
+
+college = pd.read_csv('./data/college.csv')
+college[college['STABBR'] == 'TX'].head()
+
+college2 = college.set_index('STABBR')
+college2.loc['TX'].head()
+
+# %timeit college[college['STABBR'] == 'TX']
+# %timeit college2.loc['TX']
+# %timeit college2 = college.set_index('STABBR')
+
+states = ['TX', 'CA', 'NY']
+college[college['STABBR'].isin(states)].head()
+college2.loc[states].head()
+
+# select with monotonic index
+
+college = pd.read_csv('./data/college.csv')
+college2 = college.set_index('STABBR')
+college2.index.is_monotonic
+college3 = college2.sort_index()
+college3.index.is_monotonic
+
+# %timeit college[college['STABBR']=='TX']
+# %timeit college2.loc['TX']
+# %timeit college3.loc['TX']
+
+college_unique = college.set_index('INSTNM')
+college_unique.index.is_unique
+college[college['INSTNM'] == 'Stanford University']
+college_unique.loc['Stanford University']
+
+# multiple columns which is connected can be used as an index
+
+college.index = college['CITY'] + ', ' + college['STABBR']
+college = college.sort_index()
+college.head()
+
+college.loc['Miami, FL'].head()
+## %%timeit
+crit1 = college['CITY']=='Miami'
+crit2 = college['STABBR'] == 'FL'
+college[crit1 & crit2]
+# %timeit college.loc['Miami, FL']
+
+# predict Schlumberger stock value
+
+slb = pd.read_csv('./data/slb_stock.csv', index_col = 'Date', parse_dates = ['Date'])
+slb.head()
+slb_close = slb['Close']
+slb_summary = slb_close.describe(percentiles = [.1, .9])
+slb_summary
+upper_10 = slb_summary.loc['90%']
+lower_10 = slb_summary.loc['10%']
+criteria = (slb_close < lower_10) | (slb_close > upper_10)
+slb_top_bottom_10 = slb_close[criteria]
+## plotting
+slb_close.plot(color = 'black', figsize = (12, 6))
+slb_top_bottom_10.plot(marker = 'o', style = ' ', ## no line
+                       ms = 4, color = 'lightgray')
+xmin = criteria.index[0]
+xmax = criteria.index[-1]
+import matplotlib.pyplot as plt
+plt.hlines(y = [lower_10, upper_10], xmin = xmin, xmax = xmax, color = 'black')
+
+slb_close.plot(color = 'black', figsize=(12, 6))
+plt.hlines(y = [lower_10, upper_10], xmin = xmin, xmax=xmax, color = 'lightgray')
+plt.fill_between(x=criteria.index, y1=lower_10, y2=slb_close.values, color ='black')
+plt.fill_between(x=criteria.index, y1=lower_10, y2=slb_close.values, 
+                 where=slb_close < lower_10, color='lightgray')
+plt.fill_between(x=criteria.index, y1=upper_10, y2=slb_close.values, 
+                 where=slb_close > upper_10, color='lightgray')
+
+
+# SQL where
+
+
+
+
+
+
+
+
 
 
