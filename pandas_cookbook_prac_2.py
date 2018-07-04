d5d6aa22828c76c54e0a750471983928eb60f918
jakinpilla <jakinpilla@gmail.com>
2018-07-04 13:46:34 +0900

    modify prac_2 file

--- pandas_cookbook_prac_2.py
+++ pandas_cookbook_prac_2.py
@@ -66,5 +66,51 @@ def test_return_normality(stock_data):
 slb = pd.read_csv('./data/slb_stock.csv', index_col = 'Date', parse_dates = ['Date'])
 test_return_normality(slb)
 
-qs = "DEPARTMENT in @dept and GENDER == 'Female' and 80000 <= BASE_SALARY <= 120000"
+qs = "DEPARTMENT in @depts and GENDER == 'Female' and 80000 <= BASE_SALARY <= 120000"
+
+emp_filtered = employee.query(qs)
+emp_filtered[select_columns].head()
+
+top10_depts = employee.DEPARTMENT.value_counts().index[:10].tolist()
+qs = "DEPARTMENT not in @top10_depts and GENDER == 'Female'"
+employee_filtered2 = employee.query(qs)
+employee_filtered2.head()
+
+# presercve series with .where()
+movie = pd.read_csv('./data/movie.csv', index_col = 'movie_title')
+fb_likes = movie['actor_1_facebook_likes'].dropna()
+fb_likes.head()
+fb_likes.describe(percentiles=[.1, .25, .5, .75, .9]).astype(int)
+fb_likes.hist()
+
+criteria_high= fb_likes < 20000
+criteria_high.mean().round(2)
+fb_likes.where(criteria_high).head()
+fb_likes.where(criteria_high, other=20000).head()
+criteria_low = fb_likes > 300
+fb_likes_cap = fb_likes.where(criteria_high, other=20000).where(criteria_low,300)
+fb_likes_cap.head()
+len(fb_likes), len(fb_likes_cap)
+fb_likes_cap.hist()
+fb_likes_cap2 = fb_likes.clip(lower=300, upper=20000)
+fb_likes_cap2.equals(fb_likes_cap)
+
+# dataframe row mask
+movie = pd.read_csv('./data/movie.csv', index_col = 'movie_title')
+c1 = movie['title_year'] >= 2010
+c2 = movie['title_year'].isnull()
+criteria = c1 | c2
+movie.mask(criteria).head()
+movie_mask = movie.mask(criteria).dropna(how='all')
+movie_mask.head()
+movie_boolean = movie[movie['title_year'] < 2010]
+movie_mask.equals(movie_boolean)
+movie_boolean = movie[movie['title_year'] < 2010]
+movie_mask.equals(movie_boolean)
+movie_mask.shape == movie_boolean.shape
+movie_mask.dtypes == movie_boolean.dtypes
+from pandas.testing import assert_frame_equal
+assert_frame_equal(movie_boolean, movie_mask, check_dtype=False) ## no printing if not False
+
+# boolean and .loc, .iloc
 
