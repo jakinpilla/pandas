27bd27c9494b3c69b925cfc649ee420f1107977e
jakinpilla <jakinpilla@gmail.com>
2018-07-04 17:52:18 +0900

    modify prac_2 file

--- pandas_cookbook_prac_2.py
+++ pandas_cookbook_prac_2.py
@@ -5,8 +5,9 @@ Created on Tue Jul  3 10:49:49 2018
 @author: dsc
 """
 
-from os import getcwd
+from os import getcwd, chdir
 getcwd()
+chdir("C:/Users/dsc/pandas")
 import numpy as np
 import pandas as pd
 employee = pd.read_csv('data/employee.csv')
@@ -67,50 +68,63 @@ slb = pd.read_csv('./data/slb_stock.csv', index_col = 'Date', parse_dates = ['Da
 test_return_normality(slb)
 
 qs = "DEPARTMENT in @depts and GENDER == 'Female' and 80000 <= BASE_SALARY <= 120000"
-
 emp_filtered = employee.query(qs)
-emp_filtered[select_columns].head()
 
 top10_depts = employee.DEPARTMENT.value_counts().index[:10].tolist()
 qs = "DEPARTMENT not in @top10_depts and GENDER == 'Female'"
 employee_filtered2 = employee.query(qs)
 employee_filtered2.head()
 
-# presercve series with .where()
 movie = pd.read_csv('./data/movie.csv', index_col = 'movie_title')
 fb_likes = movie['actor_1_facebook_likes'].dropna()
 fb_likes.head()
-fb_likes.describe(percentiles=[.1, .25, .5, .75, .9]).astype(int)
+
+fb_likes.describe(percentiles=[.1, .25, .5, .75, .9])
 fb_likes.hist()
-
-criteria_high= fb_likes < 20000
+criteria_high = fb_likes < 20000
 criteria_high.mean().round(2)
-fb_likes.where(criteria_high).head()
-fb_likes.where(criteria_high, other=20000).head()
-criteria_low = fb_likes > 300
-fb_likes_cap = fb_likes.where(criteria_high, other=20000).where(criteria_low,300)
-fb_likes_cap.head()
-len(fb_likes), len(fb_likes_cap)
-fb_likes_cap.hist()
-fb_likes_cap2 = fb_likes.clip(lower=300, upper=20000)
-fb_likes_cap2.equals(fb_likes_cap)
 
-# dataframe row mask
-movie = pd.read_csv('./data/movie.csv', index_col = 'movie_title')
-c1 = movie['title_year'] >= 2010
-c2 = movie['title_year'].isnull()
-criteria = c1 | c2
-movie.mask(criteria).head()
-movie_mask = movie.mask(criteria).dropna(how='all')
-movie_mask.head()
-movie_boolean = movie[movie['title_year'] < 2010]
-movie_mask.equals(movie_boolean)
-movie_boolean = movie[movie['title_year'] < 2010]
-movie_mask.equals(movie_boolean)
-movie_mask.shape == movie_boolean.shape
-movie_mask.dtypes == movie_boolean.dtypes
-from pandas.testing import assert_frame_equal
-assert_frame_equal(movie_boolean, movie_mask, check_dtype=False) ## no printing if not False
+# selecting data with boolea and loc, iloc
+movie=pd.read_csv('./data/movie.csv', index_col = 'movie_title')
+c1 = movie['content_rating'] == 'G'
+c2 = movie['imdb_score'] < 4
+criteria = c1 & c2
+movie_loc = movie.loc[criteria]
+movie_loc.head()
+ovie_loc.equals(movie[criteria])
 
-# boolean and .loc, .iloc
+# we cannot use boolean series in iloc, we need to use boolean ndarray
+movie_iloc = movie.iloc[criteria.values]
+movie_iloc.equals(movie_loc)
+criteria_col = movie.dtypes == np.int64
+criteria_col.head()
+movie.loc[:, criteria_col].head()
+movie.iloc[:, criteria_col.values].head()
+cols=['content_rating', 'imdb_score', 'title_year', 'gross']
+movie.loc[criteria, cols].sort_values('imdb_score')
+col_index = [movie.columns.get_loc(col) for col in cols]
+col_index
+movie.iloc[criteria.values, col_index]
+
+a = criteria.values
+a[:5]
+len(a), len(criteria)
+movie.loc[[True, False, True], [True, False, False, True]]
+
+# Index arranging
+college = pd.read_csv('./data/college.csv')
+columns = college.columns
+columns
+columns.values
+columns[5]
+columns[[1, 8, 10]]
+columns[-7:-4]
+columns.min(), columns.max(), columns.isnull().sum()
+columns + '_A'
+columns > 'G'
+
+
+
+
+
 
