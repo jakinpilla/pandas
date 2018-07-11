3c03bacc2812446c4ab60feaa29909ff4bc9326e
unknown <jakinpilla@gmail.com>
2018-07-12 00:20:41 +0900

    prac_4 modified

--- pandas_cookbook_prac_4.py
+++ pandas_cookbook_prac_4.py
@@ -167,7 +167,7 @@ year = crime['REPORTED_DATE'].dt.year
 crime.groupby(weekday)
 crime.groupby(year)
 crime.groupby([year, weekday]) # no error 
-crime.groupby([year, weekday]).size() # error
+# crime.groupby([year, weekday]).size() # error
 
 # linear extrapolation
 criteria = crime['REPORTED_DATE'].dt.year == 2017
@@ -240,9 +240,10 @@ goal = all_data[all_data['REPORTED_DATE'] == '2017-8-31'].reset_index(drop=True)
 goal['Total_Goal'] = goal['Total'].mul(.8).astype(int)
 goal.head()
 
-pd.merge_asof(goal, all_data, left_on = 'Total_Goal', 
-              right_on = 'Total', by='OFFENSE_CATEGORY_ID', 
-              suffixes=('_Current', '_Last'))
+#pd.merge_asof(goal, all_data, left_on = 'Total_Goal', 
+#              right_on = 'Total', by='OFFENSE_CATEGORY_ID', 
+#              suffixes=('_Current', '_Last')) # error
+# error :: MergeError: incompatible merge keys [1] int32 and int64, must be the same type
 
 goal.info()
 all_data.info()
@@ -373,11 +374,8 @@ ax.legend()
 fig
 
 top10 = movie.sort_values('budget', ascending=False)\
-<<<<<<< HEAD
 .groupby('title_year')['budget']\
 .apply(lambda x : x.iloc[:10].median() / 1e6)
-=======
-.groupby('title_year')['budget'].apply(lambda x : x.iloc[:10].median() / 1e6)
 
 top10_roll = top10.rolling(5, min_periods=1).mean()
 top10_roll.tail()
@@ -464,49 +462,94 @@ df.plot(kind='kde', color = color, ax=ax1, title='KDE plot')
 df.plot(kind='box', ax=ax2, title='Boxplot')
 df.plot(kind='hist', color=color, ax=ax3, title='Histogram')
 
+# flights dataset plotting
+flights = pd.read_csv('./data/flights.csv')
+flights.head()
+flights.columns
+flights.info()
+flights.describe()
+flights['DELAYED'] = flights['ARR_DELAY'].ge(15).astype(int)
+flights.head()
+cols = ['DIVERTED', 'CANCELLED', 'DELAYED']
+flights[cols]
+flights['ON_TIME'] = 1-flights[cols].any(axis=1)  
+flights.head()     
+cols.append('ON_TIME')
+status = flights[cols].sum()
+status
 
+fig, ax_array = plt.subplots(2, 3, figsize=(18, 8))
+(ax1, ax2, ax3), (ax4, ax5, ax6) = ax_array
+fig.suptitle('2015 US Flights - Univariate Summary', size=20)
+ac = flights['AIRLINE'].value_counts()
+ac.plot(kind='barh', ax=ax1, title='Airline')
+oc = flights['ORG_AIR'].value_counts()
+oc.plot(kind='bar', ax=ax2, rot=0, title='Origin City')
+dc = flights['DEST_AIR'].value_counts().head(10)
+dc.plot(kind='bar', ax=ax3, rot=0, title='Destination City')
+status.plot(kind='bar', ax=ax4, rot=0, log=True, title='Flight Status')
+flights['DIST'].plot(kind='kde', ax=ax5, xlim=(0, 3000), title='Distance KDE')
+flights['ARR_DELAY'].plot(kind='hist', ax=ax6, title='Arrival Delay', range=(0,200))
 
+flights['SCHED_DEP'].plot(kind='box')
+hour = flights['SCHED_DEP'] // 100
+minute = flights['SCHED_DEP'] % 100
+df_date = flights[['MONTH', 'DAY']].assign(YEAR=2015, HOUR=hour, MINUTE=minute)
+df_date.head()
+flight_dep = pd.to_datetime(df_date)
+flight_dep.head()
+flights.index = flight_dep
+fc = flights.resample('W').size()
+fc.plot(figsize=(12, 3), title='Flights per Week', grid=True)
 
+fc_miss = fc.where(fc>1000)
+fc_intp = fc_miss.interpolate(limit_direction='both')
+fc_intp
+ax = fc_intp.plot(color = 'black', figsize=(16, 4))
+fc_intp[fc<500].plot(linewidth=10, grid=True, color='.8', ax=ax)
+ax.annotate(xy=(.8, .55), xytext=(.8, .77), 
+            xycoords='axes fraction', s='missing data',
+            ha='center', size=20, arrowprops=dict())
+ax.set_title('Flights per Week (Interpolated Missing Data)')
 
+flights.groupby('DEST_AIR')['DIST']\
+.agg(['mean', 'count'])\
+.query('count > 100')\
+.sort_values('mean')\
+.tail(10).plot(kind='bar', y='mean', rot=0, legend=False, 
+     title='Average Distance per Destination')
 
+fs = flights.reset_index(drop=True)[['DIST', 'AIR_TIME']]\
+.query('DIST <= 2000').dropna()
 
+fs.plot(x='DIST', y='AIR_TIME', kind='scatter', s=1, figsize=(20, 15))
 
+fs['DIST_GROUP'] = pd.cut(fs['DIST'], bins=range(0, 2001, 250))
+fs['DIST_GROUP'].value_counts().sort_index()
+normalize = lambda x : (x - x.mean()) / x.std()
+fs['TIME_SCORE'] = fs.groupby('DIST_GROUP')['AIR_TIME']\
+.transform(normalize)
+fs.head()
+ax = fs.boxplot(by='DIST_GROUP', column='TIME_SCORE', figsize = (16,4))
+ax.set_title('Z_scores for Distance Groups')
+ax.figure.suptitle('')
 
+fs[fs['TIME_SCORE'] > 6].index
+flights.iloc[fs[fs['TIME_SCORE'] > 6].index]
+outliers = flights.iloc[fs[fs['TIME_SCORE'] > 6].index]
+outliers = outliers[['AIRLINE', 'ORG_AIR', 'DEST_AIR', 'AIR_TIME', 'DIST', 'ARR_DELAY', 'DIVERTED']]
+outliers['PLOT_NUM'] = range(1, len(outliers) + 1)
+outliers
 
-
-
->>>>>>> 348d3582eaecc0febd3715c8583b0e6166b072ee
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ax = fs.plot(x='DIST', y='AIR_TIME', kind='scatter', s=1, figsize=(16, 4), table=outliers)
+outliers.plot(x='DIST', y='AIR_TIME', kind='scatter', s=25, ax=ax, grid=True)
+outs = outliers[['AIR_TIME', 'DIST', 'PLOT_NUM']]
+for t, d, n in outs.itertuples(index=False):
+    ax.text(d+5, t+5, str(n))
+plt.setp(ax.get_xticklabels(), y=.1)
+plt.setp(ax.get_xticklines(), visible=False)
+ax.set_xlabel('')
+ax.set_title('Flight Time vs Distance with Outliers')
 
 
 
